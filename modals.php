9866381270a855f278e4305220cab820c4b22d39
Xyl <fortunebalindan@gmail.com>
2022-08-18 22:41:02 +0800

    Add Employee modal UI done

--- modals.php
+++ modals.php
@@ -3,53 +3,49 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="employeeTitle">Modal title</h5>
+                <h5 class="modal-title" id="employeeTitle">Add employee</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                 </button>
             </div>
-            <form action="#">
-                <div class="modal-body">
-                    <div class="col-lg-6">
-                        <label for="firstname" class="control-label">First name</label>
+            <div class="modal-body">
+                <form class="row g-3" action="#">
+                    <div class="col-md-6 form-floating">
                         <input type="text" class="form-control" id="firstName" name="firstname" required>
+                        <label for="firstName">First name</label>
                     </div>
-                    <div class="col-lg-6">
-                        <label for="lastname" class="control-label">Last name</label>
+                    <div class="col-md-6 form-floating">
                         <input type="text" class="form-control" id="lastName" name="lastname" required>
+                        <label for="lastName">Last name</label>
                     </div>
-                    <div class="form-group">
-                        <label for="lastname" class="control-label">Address</label>
-                        <textarea class="form-control tArea" rows="2" id="addressInfo" name="address"
-                            required></textarea>
+                    <div class="col form-floating">
+                        <textarea class="form-control tArea" rows="2" id="addressInfo" name="address" required></textarea>
+                        <label for="addressInfo">Address</label>
                     </div>
-                    <div class="form-group">
-                        <label for="lastname" class="control-label">Birthdate</label>
-                        <input type="text" class="form-control" id="birthDate" name="birthdate" required>
+                    <div class="col-md-6 form-floating">
+                        <input type="date" class="form-control" id="birthDate" name="birthdate" required>
+                        <label for="birthDate">Birthdate</label>
                     </div>
-                    <div class="form-group">
-                        <label for="lastname" class="control-label">Contact Info</label>
+                    <div class="col-md-6 form-floating">
                         <input type="number" class="form-control" id="contactInfo" name="contactinfo" required>
+                        <label for="contactInfo">Contact Info</label>
                     </div>
-                    <div class="form-group">
-                        <label for="lastname">Gender</label>
+                    <div class="col-md-4 form-floating">
                         <select class="form-select" id="genderSelection" name="gender" aria-label="Select gender">
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
+                        <label for="genderSelection">Gender</label>
                     </div>
-                    <div class="form-group">
-                        <label for="job">Job </label>
+                    <div class="col-12 form-floating">
                         <select class="form-control" id="jobSelection" name="job" aria-label="Select job">
                             <option value="" selected>- Select -</option>
                             <?php employeePosition();?>
                         </select>
+                        <label for="jobSelection">Job</label>
                     </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+                </form>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
         </div>
     </div>
 </div>
