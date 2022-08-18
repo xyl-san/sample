cf4c3a37b931ba373613b294aed446a9cc30fd66
sammy-vpd <sammy.vpd@gmail.com>
2022-08-18 17:54:19 +0800

    no notable changes. just saving

--- header.php
+++ header.php
@@ -1,14 +1,28 @@
-<nav class="headerNav navbar navbar-expand-lg navbar-light bg-blue">
+<nav class="navbar navbar-expand-lg navbar-light bg-blue">
     <div class="container-fluid">
         <button type="button" id="sidebarCollapse" class="btn btn-info">
             <i class="fas fa-align-left"></i>
-            <span>Toggle Sidebar</span>
         </button>
-        <div class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" data-bs-toggle="dropdown"
-                aria-expanded="false">Dropdown</a>
-            <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                <li><a class="dropdown-item" href="#">Logout</a></li>
+        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-align-justify"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Page</a>
+                </li>
             </ul>
         </div>
     </div>
