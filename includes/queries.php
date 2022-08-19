862432ba8910fec81efe9973e08b31337285d1da
stevievpd <stevie.vpd@gmail.com>
2022-08-19 13:14:59 +0800

    Added attendance table

--- includes/queries.php
+++ includes/queries.php
@@ -5,24 +5,52 @@ function employeeTable(){
   $query = $conn->query($sql);
   while($row = $query->fetch_assoc()){
     ?>
-      <tr>
-        <td><?php echo $row['photo']; ?></td>
-        <td><?php echo $row['employee_code']; ?></td>
-        <td><?php echo $row['firstname']?></td>
-        <td><?php echo $row['lastname']; ?></td>
-        <td><?php echo $row['address'] ?></td>
-        <td><?php echo $row['birthdate'] ?></td>
-        <td><?php echo $row['description'] ?></td>
-        <td><?php echo date('h:i A', strtotime($row['time_in'])).' - '.date('h:i A', strtotime($row['time_out'])); ?></td>
-        <td>
-          <button class="btn btn-success btn-sm edit btn-flat" data-id="<?php echo $row['employee_id']; ?>"><i class="fa fa-edit"></i> Edit</button>
-          <button class="btn btn-danger btn-sm delete btn-flat" data-id="<?php echo $row['employee_id']; ?>"><i class="fa fa-trash"></i> Delete</button>
-        </td>
-      </tr>
-    <?php
+  <tr>
+      <td><?php echo $row['photo']; ?></td>
+      <td><?php echo $row['employee_code']; ?></td>
+      <td><?php echo $row['firstname']?></td>
+      <td><?php echo $row['lastname']; ?></td>
+      <td><?php echo $row['address'] ?></td>
+      <td><?php echo $row['birthdate'] ?></td>
+      <td><?php echo $row['description'] ?></td>
+      <td><?php echo date('h:i A', strtotime($row['time_in'])).' - '.date('h:i A', strtotime($row['time_out'])); ?></td>
+      <td>
+          <button class="btn btn-success btn-sm edit btn-flat" data-id="<?php echo $row['employee_id']; ?>"><i
+                  class="fa fa-edit"></i> Edit</button>
+          <button class="btn btn-danger btn-sm delete btn-flat" data-id="<?php echo $row['employee_id']; ?>"><i
+                  class="fa fa-trash"></i> Delete</button>
+      </td>
+  </tr>
+<?php
   }
 }
 
+function attendanceTable(){
+  include 'conn.php';
+  $sql = "SELECT a.attendance_id, a.status, a.date, a.time_in, a.time_out, e.employee_code,e.firstname, e.lastname from attendance a INNER JOIN employees as e on  e.employee_id=a.employee_id ORDER BY date desc;";
+  $query = $conn->query($sql);
+  while($row = $query->fetch_assoc()){
+  $status = ($row['status'])?'<span class="label label-warning pull-right">ontime</span>':'<span class="label label-danger pull-right">late</span>';
+    ?>
+  <tr>
+      <td><?php echo date('M d, Y', strtotime($row['date'])); ?></td>
+      <td><?php echo $row['employee_code']; ?></td>
+      <td><?php echo $row['firstname']. ' ' .$row['lastname'];?></td>
+      <td><?php echo date('h:i A', strtotime($row['time_in'])).' '.$status?></td>
+      <td><?php echo date('h:i A', strtotime($row['time_out'])) ?></td>
+
+      <td>
+          <button class="btn btn-success btn-sm edit btn-flat" data-id="<?php echo $row['attendance_id']; ?>"><i
+                  class="fa fa-edit"></i> Edit</button>
+          <button class="btn btn-danger btn-sm delete btn-flat" data-id="<?php echo $row['attendance_id']; ?>"><i
+                  class="fa fa-trash"></i> Delete</button>
+      </td>
+  </tr>
+<?php
+  }
+}
+
+ 
 function employeePosition(){
   include 'conn.php';
   $sql = "SELECT job_id, description FROM job";
