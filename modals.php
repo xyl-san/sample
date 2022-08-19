58132364f6f0b3af2beeac81d14fe197ffea084c
sammy-vpd <sammy.vpd@gmail.com>
2022-08-19 13:15:38 +0800

    no notable changes

--- modals.php
+++ modals.php
@@ -63,3 +63,70 @@
     </div>
 </div>
 <!-- End Add Employee -->
+
+<!-- Start Edit Employee -->
+<div class="modal fade" id="editEmployee" tabindex="-1" role="dialog" aria-labelledby="employeeTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="employeeTitle">Edit employee information</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="row g-3" action="#">
+                    <input type="hidden" class="employeeId" name="employee_id">
+                    <div class="col-md-6 form-floating">
+                        <input type="text" class="form-control" id="firstName" name="firstname" required>
+                        <label for="firstName">First name</label>
+                    </div>
+                    <div class="col-md-6 form-floating">
+                        <input type="text" class="form-control" id="lastName" name="lastname" required>
+                        <label for="lastName">Last name</label>
+                    </div>
+                    <div class="col form-floating">
+                        <textarea class="form-control tArea" rows="2" id="addressInfo" name="address"
+                            required></textarea>
+                        <label for="addressInfo">Address</label>
+                    </div>
+                    <div class="col-md-6 form-floating">
+                        <input type="date" class="form-control" id="birthDate" name="birthdate" required>
+                        <label for="birthDate">Birthdate</label>
+                    </div>
+                    <div class="col-md-6 form-floating">
+                        <input type="number" class="form-control" id="contactInfo" name="contactinfo" required>
+                        <label for="contactInfo">Contact Info</label>
+                    </div>
+                    <div class="col-md-6 form-floating">
+                        <select class="form-select" id="genderSelection" name="gender" aria-label="Select gender">
+                            <option value="" selected>- Select -</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                        <label for="genderSelection">Gender</label>
+                    </div>
+                    <div class="col-12 form-floating">
+                        <select class="form-control" id="departmentSelection" name="department" aria-label="Select department">
+                            <option value="" selected>- Select -</option>
+                            <?php employeeDepartment();?>
+                        </select>
+                        <label for="departmentSelection">Department</label>
+                    </div>
+                    <div class="col-12 form-floating">
+                        <select class="form-control" id="jobSelection" name="job" aria-label="Select job">
+                            <option value="" selected>- Select -</option>
+                            <?php employeePosition();?>
+                        </select>
+                        <label for="jobSelection">Job</label>
+                    </div>
+                    <div class="mb-2">
+                        <button type="button" data-bs-dismiss="modal" class="btn btn-primary">Cancel</button>
+                        <button type="submit" class="btn btn-primary float-end">Submit</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Edit Employee -->
