6ec8aeda85ebf7077cd8d2421b4831d5f74150f0
sammy-vpd <sammy.vpd@gmail.com>
2022-08-24 13:35:41 +0800

    added home page icons

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'includes/styles.php';?>
+</head>
+
+<body id="bodyStyle" style="background-image: url('assets/background.png');">
+
+    <div class="nav float-end">
+        <button class="btn btn-dark">
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </button>
+    </div>
+    <div class="container text-center w-50 h-50 position-absolute top-50 start-50 translate-middle"
+        >
+        <div class="row justify-content-start">
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/pos.png" alt="" style="height:75px;">
+                    <div class="o_caption">Point of Sale</div>
+                </button>
+                
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/inventory.png" alt="" style="height:75px">
+                    <div class="o_caption">Inventory</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/attendance.png" alt="" style="height:75px">
+                    <div class="o_caption">Attendance</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/accounting.png" alt="" style="height:75px">
+                    <div class="o_caption">Accounting</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Payroll</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/employees.png" alt="" style="height:75px">
+                    <div class="o_caption">Employees</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Sales</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Customer</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">CRM</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Accounts</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Invoices</div>
+                </button>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                <button class="btn" id="crm">
+                    <img src="assets/icons/sales.png" alt="" style="height:75px">
+                    <div class="o_caption">Purchase</div>
+                </button>
+            </div>
+        </div>
+    </div>
+    <?php include 'includes/scripts.php';?>
+</body>
+
+</html>
