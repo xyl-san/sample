1b0819a9d46e07973d16a8231705eed9738d7e6e
sammy-vpd <sammy.vpd@gmail.com>
2022-08-30 15:57:47 +0800

    incorporating all get rows in one php file

--- get_rows.php
+++ get_rows.php
@@ -1,12 +1,25 @@
 <?php 
-	include 'includes/conn.php';
 
-	if(isset($_POST['id'])){
+	if(isset($_POST['empRow'])){
+        include 'includes/conn.php';
 		$employee_id = $_POST['id'];
 		$sql = "SELECT e.employee_id, e.photo, e.employee_code, e.firstname, e.lastname, e.address, e.birthdate, e.contact_info, e.gender, e.delete_flag, e.created_on,d.department_id, d.department_name, j.job_id, j.description, s.time_in, s.time_out FROM employees e INNER JOIN department as d on e.department_id=d.department_id INNER JOIN job as j on e.job_id=j.job_id INNER JOIN schedules as s on e.schedule_id=s.schedule_id WHERE e.employee_id = '$employee_id'";
 		$query = $conn->query($sql);
 		$row = $query->fetch_assoc();
 
 		echo json_encode($row);
+        $conn->close();
+	}
+
+	if(isset($_POST['attRow'])){
+        include 'includes/conn.php';
+
+		$attendance_id = $_POST['id'];
+		$sql = "SELECT a.attendance_id, a.status, a.date, a.time_in, a.time_out, e.employee_code,e.firstname, e.lastname from attendance a INNER JOIN employees as e on  e.employee_id=a.employee_id WHERE a.attendance_id = '$attendance_id'";
+		$query = $conn->query($sql);
+		$row = $query->fetch_assoc();
+
+		echo json_encode($row);
+        $conn->close();
 	}
 ?>
